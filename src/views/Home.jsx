83af64d6554724b8ec3889fdf6c4f909d6d57996
Jeffrey Farías <jeffreyfarias5@gmail.com>
2023-10-11 21:19:55 -0300

    Lista Ok merge

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -4,7 +4,7 @@ import Image from 'react-bootstrap/Image';
 const Home = () => {
     return (
       <div className="text-center mt-5">
-        <h1>Bienvenido maestro Pokémon</h1>
+        <h1>Welcome Pokemon Master</h1>
           <Image className='imgHome' src="/src/assets/img/Oddish.png" alt='odish' />   
       </div>
     )
--- src/views/Pokemons.jsx
+++ src/views/Pokemons.jsx
@@ -1,12 +1,49 @@
-const Pokemons = () => {
-    return (
-      <div className="text-center">
-        <h1>Escoge un Pokémon para ver sus estadisticas</h1>
-       
-     
-   
+import { useState, useEffect } from 'react';
+
+function Pokemons({ pokeName, setPokeName }) {
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetchPokemons();
+  }, []);
+
+  const url = 'https://pokeapi.co/api/v2/pokemon';
+
+  const fetchPokemons = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      if (data.results) {
+        const nombresPokemon = data.results.map(pokemon => pokemon.name);
+        setPokemons(nombresPokemon);
+      }
+    } catch (error) {
+      console.error("Hubo un Pokerror al obtener los Pokémons:", error);
+    }
+  };
+
+  const handleAreaChange = (e) => {
+    setPokemons(e.target.value);
+  };
+
+  return (
+    <div>
+      <div className='text-center'>
+        <h1>Choose your Pokémon...</h1>
       </div>
-    )
-  }
-  
-  export default Pokemons
+
+      <div className='d-flex justify-content-center mt-4'>
+        <select className='form-select text-white pokeSelect' value={pokeName} onChange={handleAreaChange}>
+          {pokemons.map((nombrePokemon, index) => (
+            <option key={index} value={nombrePokemon}>
+              {nombrePokemon}
+            </option>
+          ))}
+        </select>
+      </div>
+    </div>
+
+  );
+}
+
+export default Pokemons;
